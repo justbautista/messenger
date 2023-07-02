7daab0ce0b1baff4352e6f4aa428f74f3d6fba8d
justin <justinbautista923@gmail.com>
2023-07-01 20:44:44 -0400

    login/logout working

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,23 +27,58 @@ const register = async (req, res) => {
 
 		return res.send({ ok: true, message: `User ${username} created!` })
 	} catch (err) {
-        return res.status(500).send({
-            ok: false,
+		return res.status(500).send({
+			ok: false,
 			error: err,
 		})
 	}
-    // try {
-    //     const v = verifyAccessToken("eJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QzIiwidG9rZW5UeXBlIjoiYWNjZXNzIiwiaWF0IjoxNjg4MTY1MjYyLCJleHAiOjE2ODgxNjYxNjJ9.cOBczBiUdDA460oc6dEKfvBj1dGeMknRK__Bdz8WvS4")
-    //     res.send(v)
-    // } catch (err) {
-    //     res.send({ ok: false, error: err })
-    // }
+	// try {
+	//     const v = verifyAccessToken("eJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QzIiwidG9rZW5UeXBlIjoiYWNjZXNzIiwiaWF0IjoxNjg4MTY1MjYyLCJleHAiOjE2ODgxNjYxNjJ9.cOBczBiUdDA460oc6dEKfvBj1dGeMknRK__Bdz8WvS4")
+	//     res.send(v)
+	// } catch (err) {
+	//     res.send({ ok: false, error: err })
+	// }
 }
 
-const login = (req, res) => {}
+const login = async (req, res) => {
+	try {
+		const { username, password } = req.body
+		const user = await User.findOne({ username: username })
 
-const logout = (req, res) => {}
+		if (!user) {
+			return res.send("User does not exist")
+		}
 
-const refreshTokens = (req, res) => {}
+		const isValid = await bcrypt.compare(password, user["password"])
 
-module.exports = { register }
+		if (!isValid) {
+			return res.send("Wrong password")
+		}
+
+		const accessToken = generateAccessToken(username)
+		const refreshToken = generateRefreshToken(username)
+
+		res.cookie("refreshToken", refreshToken, { httpOnly: true })
+		res.header("Authorization", `Bearer ${accessToken}`)
+		return res.send({ ok: true, message: `User ${username} logged in!` })
+	} catch (err) {
+		return res.send({ ok: false, error: err.message })
+	}
+}
+
+const logout = async (req, res) => {
+	try {
+		const { username } = req.body
+		const user = await User.findOneAndUpdate(
+			{ username: username },
+			{ $inc: { refreshTokenVersion: 1 } }
+		)
+
+		res.cookie("refreshToken", "", { httpOnly: true })
+		return res.send(`Successfully logged ${user.username} out!`)
+	} catch (err) {
+		return res.send({ ok: false, error: err.message })
+	}
+}
+
+module.exports = { register, login, logout }
--- server/helpers/authHelpers.js
+++ server/helpers/authHelpers.js
@@ -13,12 +13,12 @@ const generateAccessToken = (username) => {
 	)
 }
 
-const generateRefreshToken = (username) => {
+const generateRefreshToken = (username, tokenVersion) => {
 	return jwt.sign(
 		{
 			username: username,
 			tokenType: "refresh",
-			tokenVersion: 0,
+			tokenVersion: tokenVersion,
 		},
 		process.env.REFRESH_TOKEN_SECRET,
 		{
@@ -33,6 +33,30 @@ const verifyAccessToken = (token) => {
 
 const verifyRefreshToken = (token) => {
 	return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+}
+
+const checkAuth = (req, res, next) => {
+    const accessToken = req.headers["authorization"]
+    const refreshToken = req.cookies["refreshToken"]
+
+    if (!accessToken && refreshToken) {
+        // generate new refresh token and access token
+        try {
+            const verify = verifyRefreshToken(refreshToken)
+            const newAccessToken = generateAccessToken(verify["username"])
+            const newRefreshToken = generateRefreshToken(verify["username"])
+        } catch (err) {
+            // revoke refresh token
+
+        }
+        res.send({accessToken: accessToken, refreshToken: refreshToken, message: "no access token" })
+    }
+    else if (!refreshToken) {
+        // generate make them login again
+        res.send({accessToken: accessToken, refreshToken: refreshToken, message: "no refresh token" })
+    }
+    res.send({accessToken: req.headers["authorization"]})
+    next()
 }
 
 module.exports = { generateAccessToken, generateRefreshToken, verifyAccessToken, verifyRefreshToken }
