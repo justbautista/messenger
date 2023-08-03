062c5b3858418412f3f25434ca8538a791bf3d61
justin <justinbautista923@gmail.com>
2023-08-03 19:16:07 -0400

    Revert "not the best idea to change structure"
    This reverts commit b278fd6afb2009b8a63fed3a101dbf082ed5f177.

--- client/src/components/MessageBox.js
+++ client/src/components/MessageBox.js
@@ -4,10 +4,16 @@ import { generateAxiosError } from "../helpers/helpers"
 import { useChat } from "../contexts/chatContext"
 
 export default function MessageBox() {
-	const { selectedChat, messageStore, updateMessageStore } = useChat()
-	const [currentMessages, setCurrentMessages] = useState({})
+	const {
+		selectedChat,
+		messageStore,
+		addToMessageStore,
+		findChat,
+		addToMessageBox,
+	} = useChat()
+    const [currentMessages, setCurrentMessages] = useState({})
 
-	/*
+    /*
         currentMessages
         {
             chatId: chatId,
@@ -17,7 +23,7 @@ export default function MessageBox() {
         messageStore
         {
             chatId: {
-                messages: messagesObject
+                messagesObject: messagesObject
             },
             ...
         }
@@ -30,70 +36,61 @@ export default function MessageBox() {
 					return
 				}
 
-                if (currentMessages["messages"]) {
-                    updateMessageStore(selectedChat, currentMessages["messages"])
-                }
+				const chat = findChat(selectedChat)
 
-				// if no chat in message store, fetch it and add to message store
-				if (!messageStore[selectedChat]) {
+                // if no chat in message store, fetch it and add to message store
+				if (!chat) {
 					const response = await api.get(
 						`/chats/${selectedChat}/messages`,
 						{ params: { msgsLoaded: 0 } }
 					)
 
-					setCurrentMessages({
-						chatId: selectedChat,
-						messages: response.data["messages"],
-					})
-				} else {
-					// if there is a chat, fetch from message store
-					setCurrentMessages({
-						chatId: selectedChat,
-						messages: messageStore[selectedChat],
-					})
+					addToMessageStore(selectedChat, response.data["messages"])
+                    return
 				}
+
+                // if there is a chat, fetch from message store
+                console.log(chat)
 			} catch (err) {
 				console.error(generateAxiosError(err))
 			}
 		}
 
 		getMessages()
-	}, [selectedChat])
+	}, [selectedChat, messageStore])
 
-    // saves to messageStore when chat changes
-    // selectedChat > messageStore  
-    // 
+	const loadMoreMessages = async (chatId) => {
+		const chat = findChat(chatId)
+        
+        // TODO: have a check for if allmessagesareloaded for the messagebox, this only works for the messageStore
+		if (chat["messages"]["allMessagesLoaded"]) {
+			return
+		}
 
-	// const loadMoreMessages = async (chatId) => {
-	// 	const chat = findChat(chatId)
+		const response = await api.get(`/chats/${selectedChat}/messages`, {
+			params: { msgsLoaded: chat["messages"]["totalMessagesLoaded"] },
+		})
 
-	// 	// TODO: have a check for if allmessagesareloaded for the messagebox, this only works for the messageStore
-	// 	if (chat["messages"]["allMessagesLoaded"]) {
-	// 		return
-	// 	}
+		addToMessageBox(
+			chatId,
+			response.data["messages"]["messages"],
+			response.data["messages"]["totalMessagesLoaded"]
+		)
+	}
 
-	// 	const response = await api.get(`/chats/${selectedChat}/messages`, {
-	// 		params: { msgsLoaded: chat["messages"]["totalMessagesLoaded"] },
-	// 	})
-
-	// 	addToMessageBox(
-	// 		chatId,
-	// 		response.data["messages"]["messages"],
-	// 		response.data["messages"]["totalMessagesLoaded"]
-	// 	)
-	// }
-
-	// TODO: display messages, have a conditional rendering load more messages button, implement new state for messagebox
+    // TODO: display messages, have a conditional rendering load more messages button, implement new state for messagebox
 	return (
 		<div>
-			{currentMessages["messages"] ? (
-                <div>
-                    <p>{currentMessages["chatId"]}</p>
-                    <p>{currentMessages["messages"]["allMessagesLoaded"]}</p>
-                    <p>{currentMessages["messages"]["totalMessagesLoaded"]}</p>
-                </div>
+			{messageStore.length > 0 ? (
+				messageStore.map((chat) => (
+					<div key={chat["chatId"]}>
+						<p>---------------</p>
+						<p>{chat["chatId"]}</p>
+						<p>---------------</p>
+					</div>
+				))
 			) : (
-				<p>Loading messages</p>
+				<div>Nothing in store</div>
 			)}
 		</div>
 	)
