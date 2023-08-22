657b46396cadf9ed6f885fe74bf1ba5917931c69
justin <justinbautista923@gmail.com>
2023-08-21 20:56:12 -0400

    hosting problems as always

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./components/App"
-import { ChatProvider } from "contexts/ChatContext"
-import { AuthProvider } from "contexts/AuthContext"
+import { ChatProvider } from "../contexts/ChatContext"
+import { AuthProvider } from "../contexts/AuthContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
